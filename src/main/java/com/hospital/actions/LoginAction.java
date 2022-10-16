01b257547685e48f73d5aac44a5768d9979c7908
bonfotieno <bonnyotieno9@gmail.com>
2022-10-16 23:40:32 +0300

    added login.jsp and made some changes to LoginAction.java

--- src/main/java/com/hospital/actions/LoginAction.java
+++ src/main/java/com/hospital/actions/LoginAction.java
@@ -1,7 +1,6 @@
 package com.hospital.actions;
 
 import com.hospital.model.Admin;
-import com.hospital.model.Department;
 import com.hospital.model.Patient;
 import org.apache.commons.codec.digest.DigestUtils;
 
@@ -9,20 +8,15 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 
 @WebServlet(urlPatterns = "/login")
 public class LoginAction extends Header {
@@ -34,7 +28,6 @@ public class LoginAction extends Header {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter wr = resp.getWriter();
         String userRole = req.getParameter("userrole");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
@@ -42,28 +35,31 @@ public class LoginAction extends Header {
         password = DigestUtils.md5Hex(password); //convert the password to md5 hash
 
         if (userRole.equalsIgnoreCase("Select User")) {
-            wr.print(this.loginView("You must select a user type<br/>"));
+            servletCtx.setAttribute("loginError" , "You must select a user type<br/>");
+            resp.sendRedirect("./login.jsp");
             return;
         }
 
         if (password == null || password.equalsIgnoreCase("")) {
-            wr.print(this.loginView("Password is required<br/>"));
+            servletCtx.setAttribute("loginError" , "Password is required<br/>");
+            resp.sendRedirect("./login.jsp");
             return;
         }
         if (userRole.equals("admin")) {
             Admin user = this.loginAdmin(email, password);
             if (user == null || user.getId() == null) {
-                wr.print(this.loginView("Invalid username & password combination<br/>"));
+                servletCtx.setAttribute("loginError" , "Invalid username & password combination<br/>");
+                resp.sendRedirect("./login.jsp");
                 return;
             }
         }else{
             Patient user = this.loginPatient(email, password);
             if (user == null || user.getId() == null) {
-                wr.print(this.loginView("Invalid username & password combination<br/>"));
+                servletCtx.setAttribute("loginError" , "Invalid username & password combination<br/>");
+                resp.sendRedirect("./login.jsp");
                 return;
             }
         }
-
 
         HttpSession session = req.getSession(true);
         session.setAttribute("username", email);
@@ -73,46 +69,6 @@ public class LoginAction extends Header {
         dispatcher.forward(req, resp);
     }
 
-    public String loginView(String actionError) {
-        return header(false) + "<div class=\"row \">"
-                + "<div class=\"col-md-12\">"
-                + "<br /><br /><br /><br /><br /><br /><br /><br />"
-                + "<div class=\"panel panel-default login\">"
-                + "<div class=\"panel-heading logintitle\">Login</div>"
-                + "<div class=\"panel-body\">"
-                + "<form class=\"form-horizontal center-block\" role=\"form\" action=\"./login\" method=\"post\">"
-                + "<input type=\"hidden\" name=\"action\" value=\"login\">"
-                + "<div class=\"input-group input-group-lg\">"
-                + "<span class=\"input-group-addon\" id=\"sizing-addon1\"><span class=\"glyphicon glyphicon-user\" aria-hidden=\"true\"></span></span>"
-                + "<select class=\"form-control\" name=\"userrole\">"
-                + "<option selected=\"selected\">Select User</option>"
-                + "<option value=\"admin\">Admin</option>"
-                + "<option value=\"patient\">Patient</option>"
-                + "</select>"
-                + "</div><br/>"
-                + "<div>"
-                + "</div>"
-                + "<div class=\"input-group input-group-lg\">"
-                + "<span class=\"input-group-addon\" id=\"sizing-addon1\"><span class=\"glyphicon glyphicon-envelope\" aria-hidden=\"true\"></span></span>"
-                + "<input type=\"email\" class=\"form-control\" name=\"email\" placeholder=\"example@gmail.com\" required aria-describedby=\"sizing-addon1\">"
-                + "</div>"
-                + "<br />"
-                + "<div class=\"input-group input-group-lg\">"
-                + "<span class=\"input-group-addon\" id=\"sizing-addon1\"><span class=\"glyphicon glyphicon-lock\" aria-hidden=\"true\"></span></span>"
-                + "<input type=\"password\" name=\"password\" class=\"form-control\" placeholder=\"Password\" required aria-describedby=\"sizing-addon1\">"
-                + "</div>"
-                + "<br />"
-                + "<div class=\"col-sm-7 col-sm-offset-2\">"
-                + "<button type=\"submit\" class=\"btn btn-primary btn-block btn-lg\">Login</button>"
-                + "</div>"
-                + "</form>"
-                + "</div>"
-                + "<div style=\"text-align:center;font-weight:bold;color:red\">" + (actionError != null ? actionError : "") + "</div>"
-                + "<a href=\"./register\" style=\"text-align:Center;font-weight:bold;font-size:120%;padding: 0 2%\">Register As Patient</a>"
-                + "</div>"
-                + "</div>"
-                + "</div>" + Footer.footer();
-    }
     public Admin loginAdmin(String email, String password) {
         Admin user = null;
         try {
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,40 @@
+<jsp:include page="header.jsp" />
+<div class="row">
+    <div class="col-md-12">
+        <br /><br /><br /><br /><br /><br /><br /><br />
+        <div class="panel panel-default login">
+            <div class="panel-heading logintitle">Login</div>
+            <div class="panel-body">
+                <form class="form-horizontal center-block" role="form" action="./login" method="post">
+                    <input type="hidden" name="action" value="login">
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon" id="sizing-addon1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>
+                        <select class="form-control" name="userrole">
+                            <option selected="selected">Select User</option>
+                            <option value="admin">Admin</option>
+                            <option value="patient">Patient</option>
+                        </select>
+                    </div><br/>
+                    <div>
+                    </div>
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon" id="sizing-addon1"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span></span>
+                        <input type="email" class="form-control" name="email" placeholder="example@gmail.com" required aria-describedby="sizing-addon1">
+                    </div>
+                    <br/>
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon" id="sizing-addon1"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></span>
+                        <input type="password" name="password" class="form-control" placeholder="Password" required aria-describedby="sizing-addon1">
+                    </div>
+                    <br/>
+                    <div class="col-sm-7 col-sm-offset-2">
+                        <button type="submit" class="btn btn-primary btn-block btn-lg">Login</button>
+                    </div>
+                </form>
+            </div>
+            <div style="text-align:center;font-weight:bold;color:red"> <%= application.getAttribute("loginError") %> </div>
+            <a href="./register" style="text-align:Center;font-weight:bold;font-size:120%;padding: 0 2%">Register As Patient</a>
+        </div>
+    </div>
+</div>
+<jsp:include page="footer.jsp" />
