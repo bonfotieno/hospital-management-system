9579b0e291959b2bf8cf13ed522c9bcb1e93ff3c
bonfotieno <bonnyotieno9@gmail.com>
2022-10-27 18:35:27 +0300

    added dependency injector - @Resource

--- src/main/java/com/hospital/listeners/AppContextListener.java
+++ src/main/java/com/hospital/listeners/AppContextListener.java
@@ -2,6 +2,7 @@ package com.hospital.listeners;
 
 import com.zaxxer.hikari.HikariDataSource;
 
+import javax.annotation.Resource;
 import javax.naming.InitialContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -14,15 +15,14 @@ import java.sql.DriverManager;
 @WebListener
 public class AppContextListener implements ServletContextListener {
 
+    @Resource(lookup = "java:jboss/datasources/hospital_sys")
+    DataSource dataSource;
     public void contextInitialized(ServletContextEvent ctxe) {
         System.out.print("eHospital Application Starting up....initializing default attributes");
         ServletContext ctx = ctxe.getServletContext();
         ctx.setAttribute("applicationLabel", "Botien | Online Hospital Management System");
         try {
             System.out.print("Establishing connections....");
-            InitialContext ictx = new InitialContext();
-            DataSource dataSource = (DataSource) ictx.lookup("java:jboss/datasources/hospital_sys");
-
             Connection connection = dataSource.getConnection();
             ctx.setAttribute("dbConnection", connection);
             System.out.print("Connection Established....");
